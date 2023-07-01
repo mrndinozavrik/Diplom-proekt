7f7e94fa95e2fcf6dead8438fcaf2d21876bf85f
mrndinozavrik <basketbollmarina5@gmail.com>
2023-07-01 23:39:24 +0300

    Initial Commit

---
+++ Diplom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diplom
+//
+//  Created by 마리나 on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
